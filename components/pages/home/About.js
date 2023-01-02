43d81a078e766f1ffb5c201ffed070937384b05e
Beke Ijobra <ijobra20@gmail.com>
2023-01-02 19:33:12 +0100

    added border line to about

--- components/pages/home/About.js
+++ components/pages/home/About.js
@@ -11,12 +11,12 @@ import KingdomExpression from "./KingdomExpression";
 const About = () => {
   return (
     <div className=" py-20 text-gray-700">
-      <div className="container mx-auto">
+      <div className="container mx-auto border-b-2">
         <div className="flex flex-col justify-center text-white">
           <h1 className="text-center md:text-4xl mb-10 text-2xl text-yellow-500">
             My Kingdom Expressions
           </h1>
-          <div className="mx-5 grid grid-cols-1 gap-10 md:grid-cols-3">
+          <div className="mx-5 py-5 grid grid-cols-1 gap-10 md:grid-cols-3 border-b-2">
             <KingdomExpression
               icon={<FaPeopleArrows className="text-yellow-700 text-5xl" />}
             />
